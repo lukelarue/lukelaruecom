4d6a65a391511d8009a8129ce4cdbf344ebc1f5a
Luke LaRue <larvelarue@gmail.com>
2025-10-15 16:43:42 -0500

    Revert "update run in dev"
    This reverts commit 93b7bbe0748d4b16eb7b8e97dbc89725671ba5e0.

--- apps/web/src/utils/env.ts
+++ apps/web/src/utils/env.ts
@@ -1,8 +1,6 @@
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? '/api';
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID ?? '';
-const authMock = import.meta.env.VITE_AUTH_MOCK
-  ? import.meta.env.VITE_AUTH_MOCK === 'true'
-  : import.meta.env.DEV;
+const authMock = import.meta.env.VITE_AUTH_MOCK === 'true';
 
 export const env = {
   apiBaseUrl,
