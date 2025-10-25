59ef783b0aef9125b9a4d4665a0f5d99c20b6353
Luke LaRue <larvelarue@gmail.com>
2025-10-24 22:17:57 -0500

    deploy need workloadidentity user

--- infra/workload_identity.tf
+++ infra/workload_identity.tf
@@ -30,6 +30,12 @@ resource "google_service_account_iam_member" "terraform_wif" {
   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repository}"
 }
 
+resource "google_service_account_iam_member" "deploy_automation_wif" {
+  service_account_id = google_service_account.deploy_automation.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repository}"
+}
+
 output "workload_identity_pool_name" {
   value = google_iam_workload_identity_pool.github.name
 }
